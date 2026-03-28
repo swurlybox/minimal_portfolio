7d7d9561075585cd62a08405dbd4cabfa7d90dae
Edwin Wu <wuedwin03@gmail.com>
2026-03-27 22:50:19 -0700

    Updated pmp project card description
    - as of 3/27/26

--- js/dummy.js
+++ js/dummy.js
@@ -3,16 +3,43 @@ const cards = document.getElementsByClassName("card");
 const popup_widget_bg = document.getElementById("popup-widget-background");
 const popup_widget_close = document.getElementById("widget-close-button");
 
-console.log(cards);
-console.log(popup_widget);
-console.log(popup_widget_bg);
-console.log(popup_widget_close);
+const rhd = document.getElementById("recipehub");
+const bslbd = document.getElementById("bsliarsbar");
+const lfrd = document.getElementById("lfr");
+const mp3d = document.getElementById("mp3");
 
+let state;
 let intervalId;
 let op = 0;
 
+const card_select = {
+    RECIPEHUB: 0,
+    LIARSBAR: 1,
+    LFR: 2,
+    MP3: 3,
+}
+
+console.log(card_select);
+
 for (const el of cards) {
     el.addEventListener("click", () => {
+        /* render description*/
+        rhd.style.display = "none";
+        bslbd.style.display = "none";
+        lfrd.style.display = "none";
+        mp3d.style.display = "none";
+
+        if (el.id == "recipehub-card") {
+            rhd.style.display = "block";
+        } else if (el.id == "bsliarsbar-card") {
+            bslbd.style.display = "block";
+        } else if (el.id == "lfr-card") {
+            lfrd.style.display = "block";
+        } else if (el.id == "mp3-card") {
+            mp3d.style.display = "block";
+        }
+
+        /* fade in widget*/
         popup_widget.style.display = "flex";
         popup_widget_bg.style.display = "block";
         popup_widget.style.opacity = op;
@@ -29,7 +56,8 @@ for (const el of cards) {
     });
 };
 
-popup_widget_close.addEventListener("click", () => {
+/* fadeout */
+const fadeout_widget = () => {
     console.log("Clicked on widget, closing it.");
     intervalId = setInterval(() => {
         if (popup_widget.style.opacity <= 0) {
@@ -42,19 +70,7 @@ popup_widget_close.addEventListener("click", () => {
         popup_widget.style.opacity = op;
         popup_widget_bg.style.opacity = op;
     }, 5);
-});
+}
 
-popup_widget_bg.addEventListener("click", () => {
-    console.log("Clicked on widget background, closing it."); 
-    intervalId = setInterval(() => {
-        if (popup_widget.style.opacity <= 0) {
-            popup_widget.style.display = "none";
-            popup_widget_bg.style.display = "none";
-            clearInterval(intervalId);
-            return;
-        }
-        op = op - 0.02;
-        popup_widget.style.opacity = op;
-        popup_widget_bg.style.opacity = op;
-    }, 5);
-});
+popup_widget_close.addEventListener("click", fadeout_widget);
+popup_widget_bg.addEventListener("click", fadeout_widget);
